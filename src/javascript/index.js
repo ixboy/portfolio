bbedd4e17af6d3c33f804e71a9eca3c836929d22
ixboy <ismaelixboy@gmail.com>
2021-09-26 23:36:31 +0100

    edit src javascript files

---
+++ src/javascript/index.js
@@ -0,0 +1,7 @@
+import '@fortawesome/fontawesome-free/js/fontawesome.js';
+import '@fortawesome/fontawesome-free/js/solid.js';
+import '@fortawesome/fontawesome-free/js/regular.js';
+import '@fortawesome/fontawesome-free/js/brands.js';
+import '../style.scss';
+import './main.js';
+
