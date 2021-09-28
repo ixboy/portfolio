7fce4f8c4de4a3a557e93b77249481684c7daef2
ixboy <ismaelixboy@gmail.com>
2021-09-28 05:37:09 +0100

    remove unused code

--- dist/main.js
+++ dist/main.js
@@ -1,56 +1,23 @@
-$(document).ready(function(){
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+var typed = new Typed(".typing", {
+    strings: ["Full-Stack Developer", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
-
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Full-Stack Developer", "Pen-Tester", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    var typed = new Typed(".typing-2", {
-        strings: ["Full-Stack Developer", "Pen-Tester", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
+var typed = new Typed(".typing-2", {
+    strings: ["Full-Stack Developer", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+$(function(){
+  var navbar = $('.header-inner');
+  $(window).scroll(function(){
+    if($(window).scrollTop() <=40){
+      navbar.removeClass('navbar-scroll');
+    }else{
+      navbar.addClass('navbar-scroll');
+    }
+  });
 });
